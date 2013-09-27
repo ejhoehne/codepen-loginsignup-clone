9b932b597d4b0ac60425ba184c451f3b53ed06c7
Emily <ejhoehne@gmail.com>
2013-09-26 20:21:26 -0600

    includes information for part 1

--- script.js
+++ script.js
@@ -31,6 +31,86 @@ Reponse Objects:
 
 $('document').ready(function() {
 
-	//put code here
+	$('nav a:nth-child(1)').click(function(){
+		if(!$(this).hasClass('active')) {
+			$('.signup-form').css('display', 'none'); 
+			$('nav a:nth-child(2)').removeClass('active'); 
+			$('.login-form').css('display', 'block');
+			$(this).addClass('active'); 
+		}
+	});
+	
+	$('nav a:nth-child(2)').click(function() {
+		if(!$(this).hasClass('active')) {
+			$('.signup-form').css('display', 'block');
+			$('nav a:nth-child(1)').removeClass('active');  
+			$('.login-form').css('display', 'none'); 
+			$(this).addClass('active');
+		}
+	});
+
+	$('.login-form button').on('click', function(){
+		var loginUser= {
+			username: '',
+			password: ''
+		}, 
+			password,
+			username, 
+			response; 
+
+		username=$('#login-email-field').val(); 
+		password=$('#login-password-field').val(); 
+
+		loginUser['username']= username;
+		loginUser['password']= password; 
+
+		response= codepen.api.login(loginUser); 
+
+		if(!response.success) {
+			$(".error-message").html(response.error);
+		};
+	});
+
+	$('.signup-form button').on('click', function(){
+		var signupUser= {
+			name: '', 
+			email:'', 
+			username:'', 
+			password:''
+		},
+			password, 
+			username, 
+			email, 
+			name, 
+			response; 
+
+		username=$('#signup-username-field').val();
+		password=$('#signup-password-field').val();
+		email=$('#signup-email-field').val();
+		name=$ ('#signup-name-field').val();
+
+		signupUser['username']=username;
+		signupUser['password']=password;
+		signupUser['email']=email; 
+		signupUser['name']=name;
+		
+		response=codepen.api.signup(signupUser);
+
+		if(!response.success) {
+			$(".error-message").html(response.error);
+		};
+
+
+	});
+
+
+
 
 });
+
+
+
+
+
+
+
