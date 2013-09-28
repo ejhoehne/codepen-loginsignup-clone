bd6e6b475c8c92ceab960da83107bfa0e4a061a6
Emily <ejhoehne@gmail.com>
2013-09-28 13:42:19 -0600

    Adds login and signup buttons

--- script.js
+++ script.js
@@ -49,62 +49,82 @@ $('document').ready(function() {
 		}
 	});
 
-	$('.login-form button').on('click', function(){
-		var loginUser= {
-			username: '',
-			password: ''
-		}, 
-			password,
+	 	codepen.api.login.user = {
+		username : null,
+		password : null,
+		setLogin : function(uname, upass) {
+			this.username = uname;
+			this.password = upass;
+		},
+		is_logged_in : function() {
+			if (!response.success) {
+				alert("You are not logged in. Please try again.");
+			};
+		}
+	}
+
+	$('.login-form button').on('click',function(){
+		var password,
 			username, 
 			response; 
 
 		username=$('#login-email-field').val(); 
 		password=$('#login-password-field').val(); 
+ 
+		var myUser = codepen.user;
+		myUser.setLogin(username, password);
 
-		loginUser['username']= username;
-		loginUser['password']= password; 
-
-		response= codepen.api.login(loginUser); 
+		response= codepen.api.login(myUser); 
 
 		if(!response.success) {
 			$(".error-message").html(response.error);
 		};
 	});
 
-	$('.signup-form button').on('click', function(){
-		var signupUser= {
-			name: '', 
-			email:'', 
-			username:'', 
-			password:''
-		},
+	codepen.api.signup.user = {
+		username : null,
+		password : null,
+		email : null, 
+		name : null,
+		setSignup : function(uname, upass, uemail, ufullname) {
+			this.username = uname;
+			this.password = upass;
+			this.email = uemail; 
+			this.name = ufullname;
+		}, 
+		is_logged_in : function() {
+			if (!response.success) {
+				alert("You are not logged in. Please try again.");
+			};
+		}
+	}
+
+	$('.signup-form button').on('click',function(){
+		var username, 
 			password, 
-			username, 
 			email, 
 			name, 
 			response; 
-
+  
 		username=$('#signup-username-field').val();
 		password=$('#signup-password-field').val();
 		email=$('#signup-email-field').val();
 		name=$ ('#signup-name-field').val();
 
-		signupUser['username']=username;
-		signupUser['password']=password;
-		signupUser['email']=email; 
-		signupUser['name']=name;
+		var myUser = codepen.user;
+		myUser.setSignup(username, password, email, name);
 		
-		response=codepen.api.signup(signupUser);
+		response=codepen.api.signup(myUser);
 
 		if(!response.success) {
 			$(".error-message").html(response.error);
+
 		};
 
 
 	});
 
-
-
+	console.log("Yes!");
 
 });
 
